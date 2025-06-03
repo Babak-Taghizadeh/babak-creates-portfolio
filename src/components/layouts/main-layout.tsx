ce6cec7a5d3334799cabbd813f0554bd38a09343
Babak Taghizadeh <babak.tz98@gmail.com>
2025-06-03 19:05:54 +0330

    feat: update MainLayout to import Header from a subdirectory and enhance background styling for improved visual design

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "./footer";
-import Header from "./header";
+import Header from "./header/header";
 import { memo } from "react";
 
 interface MainLayoutProps {
@@ -8,7 +8,7 @@ interface MainLayoutProps {
 
 const MainLayout = memo(({ children }: MainLayoutProps) => {
   return (
-    <div className="bg-background grid min-h-dvh grid-rows-[auto_1fr_auto]">
+    <div className="bg-background grid min-h-dvh grid-rows-[auto_1fr_auto] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:24px_24px]">
       <Header />
       <main className="relative isolate">
         <div className="mx-auto w-full max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8 xl:px-12">
