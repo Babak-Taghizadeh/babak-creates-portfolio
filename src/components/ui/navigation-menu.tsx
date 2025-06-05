b70cef78f5465797d979ffd240d15124c7aec7aa
Babak Taghizadeh <babak.tz98@gmail.com>
2025-06-05 16:47:25 +0330

    fix: update navigation menu trigger style to use transparent background for improved UI consistency

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -59,7 +59,7 @@ function NavigationMenuItem({
 }
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background p-2 text-sm font-medium  hover:text-primary disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:bg-accent/50 outline-none duration-150 transition-[color,box-shadow] ",
+  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent p-2 text-sm font-medium  hover:text-primary disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:bg-accent/50 outline-none duration-150 transition-[color,box-shadow] ",
 );
 
 function NavigationMenuTrigger({
