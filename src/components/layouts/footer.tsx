4928091119fbdd542c4640bde75a99d60fdaff4d
Babak Taghizadeh <babak.tz98@gmail.com>
2025-06-09 18:51:19 +0330

    feat: wrap social media icons in links for improved accessibility and navigation

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import GithubIcon from "../../../public/icons/github-mark.svg";
 import LinkedinIcon from "../../../public/icons/linkedin-mark.svg";
+import Link from "next/link";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -9,8 +10,22 @@ const Footer = () => {
     <footer className="text-muted-foreground space-y-4 p-4 pb-10 text-center">
       <p>&copy; {currentYear} Babak Taghizadeh. All rights reserved.</p>
       <div className="flex items-center justify-center gap-4">
-        <Image src={GithubIcon} alt="GitHub" width={20} height={20} />
-        <Image src={LinkedinIcon} alt="LinkedIn" width={20} height={20} />
+        <Link
+          href="https://github.com/Babak-Taghizadeh"
+          target="_blank"
+          className="transition duration-150 hover:scale-110"
+          rel="noopener noreferrer"
+        >
+          <Image src={GithubIcon} alt="GitHub" width={20} height={20} />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/babak-taghizadeh/"
+          target="_blank"
+          className="transition duration-150 hover:scale-110"
+          rel="noopener noreferrer"
+        >
+          <Image src={LinkedinIcon} alt="LinkedIn" width={20} height={20} />
+        </Link>
       </div>
     </footer>
   );
