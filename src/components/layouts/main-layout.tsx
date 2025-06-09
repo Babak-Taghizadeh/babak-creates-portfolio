322a2c1b30fa2847ed08ff10b881f8e209903889
Babak Taghizadeh <babak.tz98@gmail.com>
2025-06-09 18:56:51 +0330

    refactor: update MainLayout styles for improved responsiveness and background design consistency

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -8,10 +8,10 @@ interface MainLayoutProps {
 
 const MainLayout = memo(({ children }: MainLayoutProps) => {
   return (
-    <div className="bg-background grid min-h-dvh grid-rows-[auto_1fr_auto] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:24px_24px]">
+    <div className="bg-background grid min-h-dvh grid-rows-[auto_1fr_auto] bg-[radial-gradient(var(--dot-bg)_1px,transparent_1px)] [background-size:30px_30px]">
       <Header />
       <main className="relative isolate">
-        <div className="mx-auto w-full max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8 xl:px-12">
+        <div className="mx-auto w-full max-w-screen-2xl px-4 py-8 sm:px-6 md:space-y-8 lg:px-8 lg:py-20 xl:px-12">
           {children}
         </div>
       </main>
